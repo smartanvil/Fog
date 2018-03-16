d310ec2ad35a44cacf543802f1b38ea66d6d3fa5
Santiago Bragagnolo <santiago.bragagnolo@inria.fr>
2018-03-16 16:16:13 +0100

    Choosing fst account

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-geth --datadir ./devdata   --nodiscover --mine --minerthreads 1 --maxpeers 0 --verbosity 3 --unlock "1" --password ./passwordfile  --rpcapi eth,web3,net,admin --rpc --testnet
+geth --datadir ./devdata   --nodiscover --mine --minerthreads 1 --maxpeers 0 --verbosity 3 --unlock "0" --password ./passwordfile  --rpcapi eth,web3,net,admin --rpc --testnet

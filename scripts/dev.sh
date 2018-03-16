d1b7dc1a59f339e0ecf65aa22bed667d1ba7ab44
Santiago Bragagnolo <santiago.bragagnolo@inria.fr>
2018-03-16 14:57:30 +0100

    changing keystore

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -1,8 +1,2 @@
 #!/bin/sh
-
-geth --datadir ./devdata  --dev --nodiscover    \
-  --mine --minerthreads 1 --maxpeers 0 --verbosity 3 \
-  --unlock "0,1,2,3,4,5,6,7,8,9" --password ./passwordfile \
-  --rpcapi eth,web3,net,admin --rpc 
-
-
+geth --datadir ./devdata   --nodiscover --mine --minerthreads 1 --maxpeers 0 --verbosity 3 --unlock "1" --password ./passwordfile  --rpcapi eth,web3,net,admin --rpc --testnet
